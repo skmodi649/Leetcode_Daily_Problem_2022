7abaae973f0725d135452faac7c9663ec27a58db
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-02-02 09:13:34 +0530

    Second February problem commit

---
+++ src/February/Feb_2_Find_All_Anagrams_in_a_string.java
@@ -0,0 +1,16 @@
+package February;
+
+public class Feb_2_Find_All_Anagrams_in_a_string {
+    // ************* NAIVE SOLUTION *********************
+    /*
+    1. Find all the possible anagrams of the word and store it in a ArrayList of type string
+    2. Now run a for loop from i = 0 to s.length() - p.length() and find substrings accordingly
+    3. Check if the ArrayList contains the substring or not, if it does then store the value of i in the integer type
+       ArrayList index
+    4. Now just diplay the index arraylist elements
+     */
+
+    //******************* Efficient solution ********************
+
+
+}
