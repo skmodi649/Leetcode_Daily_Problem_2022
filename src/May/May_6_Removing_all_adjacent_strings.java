99ef24ce2d65cd8431dcf1db166688728a0d2f59
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2022-05-06 22:23:12 +0530

    Removing all adjacent strings from the given string program commit

---
+++ src/May/May_6_Removing_all_adjacent_strings.java
@@ -0,0 +1,42 @@
+package May;
+
+import java.util.Stack;
+
+public class May_6_Removing_all_adjacent_strings {
+    public String removeDuplicates(String s, int k) {
+        Stack<CharCounter> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack.isEmpty() && c == stack.peek().c) {
+                if (stack.peek().count == k - 1) {
+                    stack.pop();
+                } else {
+                    stack.peek().count++;
+                }
+            } else {
+                stack.push(new CharCounter(c));
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        for (CharCounter charCount : stack) {
+            res.append(charCount);
+        }
+        return res.toString();
+    }
+}
+
+class CharCounter {
+    char c;
+    int count;
+
+    CharCounter(char c) {
+        this.c = c;
+        this.count = 1;
+    }
+
+    @Override
+    public String toString() {
+        return Character.toString(c).repeat(count);
+        // repeat method is used to generate the string that has the repeatation of given string count times
+    }
+}
