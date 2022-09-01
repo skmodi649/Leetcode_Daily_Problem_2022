9ddc404fe46b78829465c6aa3756d68573c96f9d
Surmo <skmodi20bce2835@gmail.com>
2022-09-02 00:16:46 +0530

    count good nodes of binary tree program commit

--- src/September/Sept_1.java
+++ src/September/Sept_1.java
@@ -1,13 +1,26 @@
 package September;
 
+class TreeNode1 {
+    int val;
+    TreeNode1 left;
+    TreeNode1 right;
+    TreeNode1() {}
+    TreeNode1(int val) { this.val = val; }
+    TreeNode1(int val, TreeNode1 left, TreeNode1 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class Sept_1 {
-    public int goodNodes(TreeNode root) {
+    public int goodNodes(TreeNode1 root) {
 
         if(root == null) return 0;
         return count(root, root.val);
     }
 
-    private int count(TreeNode node, int max){
+    private int count(TreeNode1 node, int max){
 
         if(node == null) return 0;
 
